928b9f200c431e8f8e5d7482176cba1263459d48
toyinsolalydia <toyinsolaydia@gmail.com>
2024-12-27 23:15:32 +0000

    updated  render.php to use server side rendering

--- wp-content/plugins/woo-ftp-block/build/blocks/display-block/index.asset.php
+++ wp-content/plugins/woo-ftp-block/build/blocks/display-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '98313e5f0efb3fcf206f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '7c50dd1235d7083be2da');
--- wp-content/plugins/woo-ftp-block/src/blocks/display-block/edit.js
+++ wp-content/plugins/woo-ftp-block/src/blocks/display-block/edit.js
@@ -1,61 +1,14 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, ToggleControl, RangeControl, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
-import { PanelBody, ToggleControl, RangeControl } from '@wordpress/components';
-
 import { useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+import ServerSideRender from '@wordpress/server-side-render';
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
 export default function Edit({ attributes, setAttributes }) {
     const { numberOfProducts, displayPrice } = attributes;
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
     const blockProps = useBlockProps();
-
-    useEffect(() => {
-        fetchProducts();
-    }, [numberOfProducts]);
-
-    const fetchProducts = async () => {
-        try {
-            const response = await apiFetch({
-                path: '/woo-ftp-block/v1/products',
-            });
-            setProducts(response);
-            setIsLoading(false);
-        } catch (err) {
-            setError(err.message);
-            setIsLoading(false);
-        }
-    };
 
     return (
         <div {...blockProps}>
@@ -76,26 +29,16 @@ export default function Edit({ attributes, setAttributes }) {
                 </PanelBody>
             </InspectorControls>
 
-            <div className="woo-ftp-products">
-                {isLoading ? (
-                    <p>{__('Loading products...', 'woo-ftp-block')}</p>
-                ) : error ? (
-                    <p className="error">{error}</p>
-                ) : (
-                    <ul className="products-list">
-                        {products.slice(0, numberOfProducts).map((product) => (
-                            <li key={product.id} className="product-item">
-                                <h4>{product.name}</h4>
-                                {displayPrice && (
-                                    <div className="price" 
-                                         dangerouslySetInnerHTML={{__html: product.price}} 
-                                    />
-                                )}
-                            </li>
-                        ))}
-                    </ul>
+            <ServerSideRender
+                block="woo-ftp-block/display-block"
+                attributes={attributes}
+                EmptyResponsePlaceholder={() => <p>{__('No products found.', 'woo-ftp-block')}</p>}
+                LoadingResponsePlaceholder={() => (
+                    <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+                        <Spinner />
+                    </div>
                 )}
-            </div>
+            />
         </div>
     );
 }
