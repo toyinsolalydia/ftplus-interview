c2fe1135e121ab1b607afc37459559a0d817930a
toyinsolalydia <toyinsolaydia@gmail.com>
2024-12-27 22:19:19 +0000

    Initialized new plugin

--- wp-content/plugins/woo-ftp-block/woo-ftp-block.php
+++ wp-content/plugins/woo-ftp-block/woo-ftp-block.php
@@ -25,6 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_woo_ftp_block_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ . '/build/blocks/display-block' );
 }
 add_action( 'init', 'create_block_woo_ftp_block_block_init' );
