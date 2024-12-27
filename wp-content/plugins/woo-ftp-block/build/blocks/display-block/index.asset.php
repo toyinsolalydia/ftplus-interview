e526b3dd187b512b9bfd2421378627729dc0610b
toyinsolalydia <toyinsolaydia@gmail.com>
2024-12-27 23:05:21 +0000

    Added render.php

--- wp-content/plugins/woo-ftp-block/build/blocks/display-block/index.asset.php
+++ wp-content/plugins/woo-ftp-block/build/blocks/display-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acfb28777299bf567582');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '98313e5f0efb3fcf206f');
---
+++ wp-content/plugins/woo-ftp-block/build/blocks/display-block/render.php
@@ -0,0 +1,8 @@
+<?
+$block_wrapper_attributes = get_block_wrapper_attributes();
+    // wp_send_json($content);
+?>
+
+<div <? echo $block_wrapper_attributes; ?> >
+    <? echo $content; ?>
+</div>  
